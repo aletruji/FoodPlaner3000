308a3220c70b34288b94242668970d2aac9065b4
aletruji <al.trujillo@proton.me>
2024-12-29 21:29:37 +0100

    first push

--- app/src/main/java/me/trujillo/foodplaner3000/MainActivity.kt
+++ app/src/main/java/me/trujillo/foodplaner3000/MainActivity.kt
@@ -5,17 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -24,17 +28,20 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.DismissDirection
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -43,11 +50,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import kotlinx.coroutines.launch
 import me.trujillo.foodplaner3000.ui.theme.FoodPlaner3000Theme
 
 
@@ -75,7 +86,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CustomText(text: String) {
-    Text(text = text, style = TextStyle(fontSize = 20.sp, color = Color.Red))
+    Text(text = text, style = TextStyle(fontSize = 20.sp, color = Red))
 }
 
 @Composable
@@ -107,69 +118,15 @@ fun BottomIconButtons() {
         }
     }
 }
-data class ShoppingItem(val id:Int,
-                        var name:String,
-                        var quantity:Int,
-                        var isEditing: Boolean = false)
 
 @SuppressLint("CoroutineCreationDuringComposition")
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp() {
-
-
-    /*var items by remember { mutableStateOf(listOf("14 Eier", "50g Mehl", "200g Fleisch", "150g Butter", "1 Brockoli",
-        "500g Mandeln", "50g Chilli", "1l Milch", "20g Curry", "500g Salat", )) } */
+    var items by remember { mutableStateOf(listOf("14 Eier", "50g Mehl",
+        "200g Fleisch", "150g Butter", "1 Brockoli", "500g Mandeln",
+        "50g Chilli", "1l Milch", "20g Curry", "500g Salat")) }
     val scope = rememberCoroutineScope()
-    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
-    var showDialog by remember { mutableStateOf(false)}
-    var itemName by remember { mutableStateOf("") }
-    var itemQuantity by remember { mutableStateOf("")}
-
-
-
-
-
-
-    if(showDialog) {
-        AlertDialog(onDismissRequest = { showDialog = false },
-            confirmButton = {
-                Row(
-                    modifier = Modifier.fillMaxWidth().padding(8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween){
-                    Button(onClick = {
-                        if (itemName.isNotBlank()){
-                            val newItem = ShoppingItem( id = sItems.size+1,
-                                name = itemName,
-                                quantity = itemQuantity.toInt())
-                            sItems = sItems + newItem
-                            showDialog  = false
-                            itemName = ""
-
-                    }
-                }){Text("Add")}
-                    Button(onClick = {showDialog = false}) {
-                        Text("Cancel")
-                    }
-
-                }},
-            title = { Text("Add Shopping Item") },
-            text = { 
-                Column {
-                    OutlinedTextField(value = itemName,
-                        onValueChange ={ itemName = it },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth().padding(16.dp))
-
-                    OutlinedTextField(value = itemQuantity,
-                        onValueChange ={ itemQuantity = it },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth().padding(16.dp))
-                }
-            })
-    
-
-        }
 
 
 
@@ -198,15 +155,15 @@ fun MyApp() {
             )
             Box(
                 modifier = Modifier
-                    .background(Color(0xFF212121), shape = RoundedCornerShape(16.dp))
+                    .background(Color(0xFF212121),shape = RoundedCornerShape(16.dp))
                     .align(Alignment.BottomStart)
                     .padding(10.dp)
             ) {
-            Text(
-                text = "Einkaufsliste...               ",
+                Text(
+                    text = "Einkaufsliste...               ",
 
-                color = Color.White
-            )
+                    color = Color.White
+                )
 
                 Box(
                     modifier = Modifier
@@ -221,7 +178,7 @@ fun MyApp() {
                         modifier = Modifier.size(15.dp) // Größe des Icons anpassen
                     )
                 }
-        }
+            }
         }
 
         Divider(
@@ -239,19 +196,64 @@ fun MyApp() {
 
 
 
-
             LazyColumn(modifier = Modifier.fillMaxSize()) {
-                // Verwende die `itemsIndexed`-Funktion, um über die Liste zu iterieren
-                items(sItems) {
-                              ShoppingListItem(it, {})
+                itemsIndexed(
+                    items = items,
+                    key = { _, item -> item }  // Verwenden des Item-Inhalts als eindeutigen Schlüssel
+                ) { index, item ->
+                    val dismissState = rememberDismissState()
+
+                    // Überwachen des `dismissState` mit `LaunchedEffect`
+                    if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                        LaunchedEffect(key1 = items) {
+                            scope.launch {
+                                items = items.toMutableList().also { list ->
+                                    list.removeAt(index)
+                                }
+                            }
+                        }
+                    }
+
+                    SwipeToDismiss(
+                        state = dismissState,
+                        directions = setOf(DismissDirection.EndToStart),
+                        background = {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(Color(0xFF212121))
+                                    .padding(16.dp)
+                            ) {
+                                Text(
+                                    text = "Delete",
+                                    color = Color( 0xFF8B0000),
+                                    modifier = Modifier.align(Alignment.CenterEnd)
+                                )
+                            }
+                        },
+            dismissContent = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .background(
+                            Color(0xff565a47),
+                            shape = MaterialTheme.shapes.medium
+                        )
+                        .padding(16.dp)
+                ) {
+                    Text(text = item, fontSize = 18.sp)
                 }
-
-
-
-
+            }
+        )
     }
+
+
+
+
+            }
             IconButton(
-                onClick = { showDialog = true },
+                onClick = { /* Aktion zum Löschen oder Schließen */ },
                 modifier = Modifier
                     .align(Alignment.BottomEnd) // Rechts platzieren
                     .padding(25.dp)
@@ -273,21 +275,15 @@ fun MyApp() {
                 .weight(0.1f)
                 .background(Color(0xFFFFA500))
         ){
-
+            /*Text(
+                text = "dritte Box",
+                modifier = Modifier.align(Alignment.Center),
+                color = Color.Black
+            )*/
             BottomIconButtons()
         }
     }
 }
-
-@Composable
-fun ShoppingListItem(
-    item: ShoppingItem,
-    onEditClick: () -> Unit
-) {
-
-}
-
-
 
 @Preview(showBackground = true)
 @Composable
---
+++ app/src/main/java/me/trujillo/foodplaner3000/model/items.kt
@@ -0,0 +1,5 @@
+package me.trujillo.foodplaner3000.model
+
+data class items (
+    val name: String,
+    val quantity: String)
